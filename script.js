0386699ae530f72f3c9bee25f398735e2c306425
AnthonyT-45 <atherrien54@gmail.com>
2024-06-04 22:58:18 -0700

    Added a dropdown menu if browser width < 1000px

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("drop-button").onclick = function () {
+  dropdownButton();
+};
+
+function dropdownButton() {
+  document.getElementById("dropdown-links").classList.toggle("show");
+}
