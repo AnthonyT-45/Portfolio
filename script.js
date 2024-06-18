9b4aaa126deac975ec2958cc438e5c9afe5db2ba
AnthonyT-45 <atherrien54@gmail.com>
2024-06-17 19:40:09 -0700

    Added dropdown menu change.
    Maybe going to fix the way
    projects are viewed.

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ document.getElementById("drop-button").onclick = function () {
 function dropdownButton() {
   document.getElementById("dropdown-links").classList.toggle("show");
 }
+
+window.onclick = function (event) {
+  if (
+    !event.target.matches("#drop-button") &&
+    !event.target.matches("#drop-image")
+  ) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
