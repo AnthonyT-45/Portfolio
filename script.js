c4c44255df211b70795f3f71212df4a9cd87cc29
AnthonyT-45 <atherrien54@gmail.com>
2024-06-19 11:01:41 -0700

    Added modals when clicking the project item.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+var modal = document.getElementById("modal");
+
+var projectItems = document.querySelectorAll(".project-item");
+
+projectItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 document.getElementById("drop-button").onclick = function () {
   dropdownButton();
 };
