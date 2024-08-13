be17494a15722dd6810a36cf6a4f49cd14ea5afd
AnthonyT-45 <atherrien54@gmail.com>
2024-08-13 14:21:30 -0700

    Test

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll(".sections");
-  const logo = document.getElementById("port-logo");
-  const aboutContainer = document.querySelector(".about-container");
-  const projectContainer = document.querySelector(".project-container");
-  const sectionContainer = document.querySelector(".sections-container");
+const sections = document.querySelectorAll(".sections");
+const logo = document.getElementById("port-logo");
+const aboutContainer = document.querySelector(".about-container");
+const projectContainer = document.querySelector(".project-container");
+const sectionContainer = document.querySelector(".sections-container");
 
-  sections.forEach((section) => {
-    section.addEventListener("click", function () {
-      if (section.id === "resume") {
-        return;
-      }
-      if (section.id === "about") {
-        setTimeout(() => {
-          aboutContainer.style.visibility = "visible";
-          aboutContainer.style.opacity = "1";
-        }, 500);
-      }
+sections.forEach((section) => {
+  section.addEventListener("click", function () {
+    if (section.id === "resume") {
+      return;
+    }
+    if (section.id === "about") {
+      setTimeout(() => {
+        aboutContainer.style.visibility = "visible";
+        aboutContainer.style.opacity = "1";
+      }, 500);
+    }
 
-      if (section.id === "projects") {
-        setTimeout(() => {
-          projectContainer.style.visibility = "visible";
-          projectContainer.style.opacity = "1";
-        }, 500);
-      }
-      sections.forEach((section) => {
-        section.style.visibility = "hidden";
-        section.style.opacity = "0";
-      });
+    if (section.id === "projects") {
+      setTimeout(() => {
+        projectContainer.style.visibility = "visible";
+        projectContainer.style.opacity = "1";
+      }, 500);
+    }
+    sections.forEach((section) => {
+      section.style.visibility = "hidden";
+      section.style.opacity = "0";
     });
   });
+});
 
-  logo.addEventListener("click", function () {
-    sections.forEach((section) => {
-      aboutContainer.style.visibility = "hidden";
-      aboutContainer.style.opacity = "0";
-      projectContainer.style.visibility = "hidden";
-      projectContainer.style.opacity = "0";
-      setTimeout(() => {
-        section.style.visibility = "visible";
-        section.style.opacity = "1";
-      }, 500);
-    });
+logo.addEventListener("click", function () {
+  sections.forEach((section) => {
+    aboutContainer.style.visibility = "hidden";
+    aboutContainer.style.opacity = "0";
+    projectContainer.style.visibility = "hidden";
+    projectContainer.style.opacity = "0";
+    setTimeout(() => {
+      section.style.visibility = "visible";
+      section.style.opacity = "1";
+    }, 500);
   });
 });
